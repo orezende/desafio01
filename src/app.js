388848c4849dab9ee1ba23cacc9e2a89299a2610
João Vitor <joaorabello99@gmail.com>
2020-06-16 19:54:36 -0300

    conclusão desafio 01

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,77 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const respositoryToCreate = {
+    url,
+    title,
+    techs,
+    likes: 0,
+    id: uuid()
+  };
+
+  repositories.push(respositoryToCreate);
+
+  return response.status(201).json(respositoryToCreate)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { url, title, techs } = request.body;
+
+  const repositoryIndexToUpdate = repositories
+    .findIndex(repository => repository.id === id);
+
+  if (repositoryIndexToUpdate < 0) {
+    return response.status(400).send();
+  }
+
+  repositories[repositoryIndexToUpdate] = {
+    url,
+    title,
+    techs,
+    id,
+    likes: repositories[repositoryIndexToUpdate].likes
+  };
+
+  return response.status(200).json(repositories[repositoryIndexToUpdate]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndexToDelete = repositories
+    .findIndex(repository => repository.id === id);
+
+  if (repositoryIndexToDelete < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repositoryIndexToDelete, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndexToAddLike = repositories
+    .findIndex(repository => repository.id === id);
+
+  if (repositoryIndexToAddLike < 0) { 
+    return response.status(400).send();
+  }
+
+  repositories[repositoryIndexToAddLike].likes =
+    repositories[repositoryIndexToAddLike].likes + 1;
+
+  return response.status(200).json(repositories[repositoryIndexToAddLike]);
 });
 
 module.exports = app;
